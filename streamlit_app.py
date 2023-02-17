4479d8177571d08a8724a40000f852b032670557
Ash-A6rams <79352638+Ash-A6rams@users.noreply.github.com>
2023-02-17 11:11:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title('My first streamlit')
-streamlit.header('Menu')
-streamlit.text('Zucchini')
-streamlit.text('Sushi')
-streamlit.text('Risotto')
-streamlit.text('Lasagne')
+streamlit.title('New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
