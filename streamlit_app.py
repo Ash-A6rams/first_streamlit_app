84d6d6b5fdfc7bd425202031ffb861c6a9a5df19
Ash-A6rams <79352638+Ash-A6rams@users.noreply.github.com>
2023-02-17 11:10:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My first streamlit')
+streamlit.header('Menu')
+streamlit.text('Zucchini')
+streamlit.text('Sushi')
+streamlit.text('Risotto')
+streamlit.text('Lasagne')
